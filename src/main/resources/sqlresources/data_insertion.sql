8dc4166310842956cc164c6c8345f33d4c365476
deszczkarolina <deszcz.karolina@gmail.com>
2019-04-09 09:05:57 +0200

    sql scripts for database schema creation and data population
    Change-Id: I569ab3b6804458851e755bb953ff7b08d4286ad9

---
+++ src/main/resources/sqlresources/data_insertion.sql
@@ -0,0 +1,173 @@
+INSERT INTO public.drivers(	pesel, first_name, last_name, email, driving_license_number)
+VALUES ('07012527358', 'Marcin','Kłos','mklos.borowik@gmail.com', 'VMS848599');
+INSERT INTO public.drivers(	pesel, first_name, last_name, email, driving_license_number)
+VALUES ('13102123397', 'Michał','Nowakowski','mnowakowski.borowik@gmail.com', 'URW132502');
+INSERT INTO public.drivers(	pesel, first_name, last_name, email, driving_license_number)
+VALUES ('41090788874', 'Bartłomiej','Kowalski','bkowalski.borowik@gmail.com', 'VTE905172');
+INSERT INTO public.drivers(	pesel, first_name, last_name, email, driving_license_number)
+VALUES ('00221232880', 'Jan','Nowak','jnowak.borowik@gmail.com', 'LRO575427');
+INSERT INTO public.drivers(	pesel, first_name, last_name, email, driving_license_number)
+VALUES ('07012527359', 'Marek','Nietoperz','mnietoperz.borowik@gmail.com', 'CUO783857');
+INSERT INTO public.drivers(	pesel, first_name, last_name, email, driving_license_number)
+VALUES ('13102123398', 'Michał','Chłopski','mchlopski.borowik@gmail.com', 'URW132503');
+INSERT INTO public.drivers(	pesel, first_name, last_name, email, driving_license_number)
+VALUES ('41090788875', 'Stefan','Niski','sniski.borowik@gmail.com', 'VTE905173');
+INSERT INTO public.drivers(	pesel, first_name, last_name, email, driving_license_number)
+VALUES ('00221232881', 'Mateusz','Wysoki','mwysoki.borowik@gmail.com', 'LRO575428');
+INSERT INTO public.drivers(	pesel, first_name, last_name, email, driving_license_number)
+VALUES ('75083101483', 'Konrad','Wolny','kwolny.borowik@gmail.com', 'VMS848490');
+INSERT INTO public.drivers(	pesel, first_name, last_name, email, driving_license_number)
+VALUES ('16211625543', 'Adam','Stefaniak','astefaniak.borowik@gmail.com', 'KRW132503');
+INSERT INTO public.drivers(	pesel, first_name, last_name, email, driving_license_number)
+VALUES ('46040469700', 'Marian','Kowalski','mkowalski.borowik@gmail.com', 'VXE906172');
+INSERT INTO public.drivers(	pesel, first_name, last_name, email, driving_license_number)
+VALUES ('18110259748', 'Piotr','Bosak','pbosak.borowik@gmail.com', 'CBY575427');
+INSERT INTO public.drivers(	pesel, first_name, last_name, email, driving_license_number)
+VALUES ('22010219924', 'Wojciech','Krosno','wkrosno.borowik@gmail.com', 'VES848599');
+INSERT INTO public.drivers(	pesel, first_name, last_name, email, driving_license_number)
+VALUES ('30101012646', 'Szymon','Workowski','sworkowski.borowik@gmail.com', 'URW2232502');
+INSERT INTO public.drivers(	pesel, first_name, last_name, email, driving_license_number)
+VALUES ('87071650165', 'Błażej','Kowalewicz','bkowalewicz.borowik@gmail.com', 'XXX905172');
+INSERT INTO public.drivers(	pesel, first_name, last_name, email, driving_license_number)
+VALUES ('93032383759', 'Bolesław','Wielski','bwielski.borowik@gmail.com', 'LRO544427');
+INSERT INTO public.drivers(	pesel, first_name, last_name, email, driving_license_number)
+VALUES ('80010317011', 'Szczepan','Kozłowski','skozlowski.borowik@gmail.com', 'VMS8485101');
+INSERT INTO public.drivers(	pesel, first_name, last_name, email, driving_license_number)
+VALUES ('18220540925', 'Tomasz','Niewiem','tniewiem.borowik@gmail.com', 'KRK132502');
+INSERT INTO public.drivers(	pesel, first_name, last_name, email, driving_license_number)
+VALUES ('70051863134', 'Mikołaj','Wiatr','mwiatr.borowik@gmail.com', 'VTR905172');
+INSERT INTO public.drivers(	pesel, first_name, last_name, email, driving_license_number)
+VALUES ('77050604713', 'Kazimierz','Adamczyk','kadamczyk.borowik@gmail.com', 'LRO57115427');
+
+
+INSERT INTO public.passengers(id, first_name, last_name, email)
+VALUES (1, 'Anna', 'Nowak', 'anowak.vip@gmail.com');
+INSERT INTO public.passengers(id, first_name, last_name, email)
+VALUES (2, 'Peter', 'Smith', 'psmith.vip@gmail.com');
+INSERT INTO public.passengers(id, first_name, last_name, email)
+VALUES (3, 'John', 'April', 'japril.vip@gmail.com');
+INSERT INTO public.passengers(id, first_name, last_name, email)
+VALUES (4, 'Artur', 'Mirkowski', 'amirkowski.vip@gmail.com');
+INSERT INTO public.passengers(id, first_name, last_name, email)
+VALUES (5, 'Marianna', 'Koralowa', 'mkoralowa.vip@gmail.com');	
+INSERT INTO public.passengers(id, first_name, last_name, email)
+VALUES (6, 'Eve', 'Williams', 'ewilliams.vip@gmail.com');
+INSERT INTO public.passengers(id, first_name, last_name, email)
+VALUES (7, 'Beata', 'Kowalczyk', 'bkowalczyk.vip@gmail.com');
+INSERT INTO public.passengers(id, first_name, last_name, email)
+VALUES (8, 'Mirosław', 'Chmielewski', 'mchmielewicz.vip@gmail.com');
+INSERT INTO public.passengers(id, first_name, last_name, email)
+VALUES (9, 'Bogusław', 'Góra', 'bgora.vip@gmail.com');
+INSERT INTO public.passengers(id, first_name, last_name, email)
+VALUES (10, 'Radosław', 'Nijaki', 'rnijaki.vip@gmail.com');
+INSERT INTO public.passengers(id, first_name, last_name, email)
+VALUES (11, 'Alicja', 'Krzyżak', 'akrzyak.vip@gmail.com');
+INSERT INTO public.passengers(id, first_name, last_name, email)
+VALUES (12, 'Tymoteusz', 'Tytus', 'ttytus.vip@gmail.com');
+INSERT INTO public.passengers(id, first_name, last_name, email)
+VALUES (13, 'Albert', 'Kot', 'akot.vip@gmail.com');
+INSERT INTO public.passengers(id, first_name, last_name, email)
+VALUES (14, 'Anastazja', 'Lisicka', 'alisicka.vip@gmail.com');
+INSERT INTO public.passengers(id, first_name, last_name, email)
+VALUES (15, 'Konstanty', 'Las', 'klas.vip@gmail.com');
+INSERT INTO public.passengers(id, first_name, last_name, email)
+VALUES (16, 'George', 'Ray', 'gray.vip@gmail.com');
+INSERT INTO public.passengers(id, first_name, last_name, email)
+VALUES (17, 'Patrycja', 'Pasek', 'ppasek.vip@gmail.com');
+INSERT INTO public.passengers(id, first_name, last_name, email)
+VALUES (18, 'Paulina', 'Wierzba', 'pwierzba.vip@gmail.com');
+INSERT INTO public.passengers(id, first_name, last_name, email)
+VALUES (19, 'Piotr', 'Szary', 'pszary.vip@gmail.com');
+INSERT INTO public.passengers(id, first_name, last_name, email)
+VALUES (20, 'Maurycy', 'Dolina', 'mdolina.vip@gmail.com');
+
+INSERT INTO public.users (driver_id, login, password) 
+VALUES ('07012527358', 'mklos', 'FE38924C1F6CBF1BABAC662820A54031');
+INSERT INTO public.users (driver_id, login, password)
+ VALUES ('13102123397', 'mnowakowski', 'C8FB39AECF91AF9566BE2EC2FCB7C1A9');
+INSERT INTO public.users (driver_id, login, password)
+ VALUES ('41090788874', 'bkowalski', 'DC5C01575C83E645EA9704B5C9685DC3');
+INSERT INTO public.users (driver_id, login, password)
+ VALUES ('00221232880', 'jnowak', 'E836E670C8B4367A1F957657621F37EB');
+INSERT INTO public.users (driver_id, login, password)
+ VALUES ('07012527359', 'mnietoperz', '2C2E762F395C77190B2289CA51D7F2F1');
+INSERT INTO public.users (driver_id, login, password)
+ VALUES ('13102123398', 'mchlopski', '0BF55C8A7ABB01F3FD5240A801DD445D');
+INSERT INTO public.users (driver_id, login, password)
+ VALUES ('41090788875', 'sniski', '50AD440B27B9A83169EC8FAAF27E7AA9');
+INSERT INTO public.users (driver_id, login, password)
+ VALUES ('00221232881', 'mwysoki', '031C5BF3B52AF55C6F88A3E085D69CF0');
+INSERT INTO public.users (driver_id, login, password) 
+VALUES ('75083101483', 'kwolny', '1B7C68C7BB91FE75AA168293686C0E47');
+INSERT INTO public.users (driver_id, login, password) 
+VALUES ('16211625543', 'astefaniak', 'BA127ADD01A5628931F4C47AA98F3B04');
+INSERT INTO public.users (driver_id, login, password) 
+VALUES ('46040469700', 'mkowalski', '6D4CBB3455A7EFE6E9FA305416319F43');
+INSERT INTO public.users (driver_id, login, password)
+ VALUES ('18110259748', 'pbosak', '99FBDA48FA36F5EF1B09CC0D10C4DE63');
+INSERT INTO public.users (driver_id, login, password)
+ VALUES ('22010219924', 'wkrosno', '3C51A2F1A112074817F560DB6E9E8E29');
+INSERT INTO public.users (driver_id, login, password)
+ VALUES ('30101012646', 'sworkowski', '531493BAEDBE208C3FFC24BBFE33551B');
+INSERT INTO public.users (driver_id, login, password)
+ VALUES ('87071650165', 'bkowalewicz', 'C46EDD57AE55E7FBA90700977C8810B1');
+INSERT INTO public.users (driver_id, login, password) 
+VALUES ('93032383759', 'bwielski', '921589CF67B8AC074E166E213F82D9FD');
+INSERT INTO public.users (driver_id, login, password)
+ VALUES ('80010317011', 'skozlowski', '05A2DF47347563902D89A66B22DD465B');
+INSERT INTO public.users (driver_id, login, password) 
+VALUES ('18220540925', 'tniewiem', '287B264C613872A2C752A849ED4ACA29');
+INSERT INTO public.users (driver_id, login, password) 
+VALUES ('70051863134', 'mwiatr', '4842EA71397A05FFBF5366B099AE696B');
+INSERT INTO public.users (driver_id, login, password)
+ VALUES ('77050604713', 'kadamczyk', '9D128E533B02B1C276A72B88873C5191');
+
+
+INSERT INTO public.cars (registration_number, mark, model, production_date) 
+VALUES ('WA1235EX', 'BMW', 'Serii 5', '2017-10-10');
+INSERT INTO public.cars (registration_number, mark, model, production_date) 
+VALUES ('GDN124RR', 'BMW', 'Serii 7', '2016-10-10');
+INSERT INTO public.cars (registration_number, mark, model, production_date) 
+VALUES ('CNA33R21', 'BMW', 'X7', '2018-03-05');
+INSERT INTO public.cars (registration_number, mark, model, production_date) 
+VALUES ('KRK123HY', 'BMW', 'X6', '2018-10-15');
+INSERT INTO public.cars (registration_number, mark, model, production_date) 
+VALUES ('WAWUUW', 'Volvo', 'S90', '2016-09-12');
+INSERT INTO public.cars (registration_number, mark, model, production_date) 
+VALUES ('CBY123QS', 'Volvo', 'S60', '2018-01-03');
+INSERT INTO public.cars (registration_number, mark, model, production_date) 
+VALUES ('SGL132DF', 'Volvo', 'XC90', '2017-09-08');
+INSERT INTO public.cars (registration_number, mark, model, production_date) 
+VALUES ('DWR741D', 'Audi', 'A6', '2017-04-05');
+INSERT INTO public.cars (registration_number, mark, model, production_date) 
+VALUES ('SG659IWQ', 'Audi', 'A8', '2015-06-07');
+INSERT INTO public.cars (registration_number, mark, model, production_date)
+ VALUES ('GDYTEQ11', 'Audi', 'Q7', '2018-12-12');
+INSERT INTO public.cars (registration_number, mark, model, production_date) 
+VALUES ('CT6651WF', 'Audi', 'Q8', '2017-10-03');
+INSERT INTO public.cars (registration_number, mark, model, production_date) 
+VALUES ('OPYT1345', 'Mercedes', 'Klasa E', '2016-09-03');
+INSERT INTO public.cars (registration_number, mark, model, production_date) 
+VALUES ('WWYTR14', 'Mercedes', 'Klasa S', '2017-07-08');
+INSERT INTO public.cars (registration_number, mark, model, production_date) 
+VALUES ('KRK4521J', 'Mercedes', 'Klasa V', '2018-03-03');
+INSERT INTO public.cars (registration_number, mark, model, production_date) 
+VALUES ('EHAK1356', 'Mercedes', 'GLS', '2019-01-01');
+INSERT INTO public.cars (registration_number, mark, model, production_date) 
+VALUES ('KIYAHDEB', 'Lexus', 'LS', '2018-04-05');
+INSERT INTO public.cars (registration_number, mark, model, production_date) 
+VALUES ('GGG11357', 'Jaguar', 'XJ', '2017-03-05');
+INSERT INTO public.cars (registration_number, mark, model, production_date) 
+VALUES ('HTEVD531', 'Porsche', 'Panamera', '2019-01-01');
+INSERT INTO public.cars (registration_number, mark, model, production_date)
+ VALUES ('FDFEW12W', 'Maserati', 'Quattroporte', '2018-04-05');
+INSERT INTO public.cars (registration_number, mark, model, production_date) 
+VALUES ('DWFGFE24', 'Bentley', 'Mulsanne', '2017-03-05');
+
+INSERT INTO public.schedule (id, driver_id, passenger_id, car_id, place_from, place_to, date_from, date_to) 
+VALUES (1, '07012527358', 1, 'WA1235EX', 'Zamek Królewski', 'Ambasada USA', '2019-03-29 15:00:00', '2019-03-29 16:00:00');
+INSERT INTO public.schedule (id, driver_id, passenger_id, car_id, place_from, place_to, date_from, date_to) 
+VALUES (2, '07012527358', 4, 'WA1235EX', 'Ambasada USA', 'Pałac Prezydencki', '2019-03-29 17:00:00', '2019-03-29 19:00:00');
+INSERT INTO public.schedule (id, driver_id, passenger_id, car_id, place_from, place_to, date_from, date_to) 
+VALUES (3, '41090788875', 14, 'WWYTR14', 'Ambasada Kanady', 'Sejm RP', '2019-03-29 17:00:00', '2019-03-29 19:00:00');
+
