7583f8f7e8af7cbce709569e4592caf8d6506bf0
kdeszcz <kdeszcz@bluesoft.net.pl>
2019-05-22 13:49:51 +0200

    adding missing files
    Change-Id: I5f7ca3d3c4b53aae46fc62b641008014ba3ed918

---
+++ src/adminPanelGui/src/app/model/passanger.ts
@@ -0,0 +1,14 @@
+export class Passanger {
+    pesel : string;
+    firstName : string;
+    lastName : string;
+    email : string;
+ 
+
+    constructor(firstName : string, lastName : string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+
+}
