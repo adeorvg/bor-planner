57946346ea47714bcea933bb090042fc37abb575
Adrian Zach <adrien997@gmail.com>
2019-05-25 13:26:33 +0200

    added exception handling for saving schedule to database
    Change-Id: I9dfdbf1b9c5599a0051fafefe8f37a7182a1ece7

--- src/main/java/com/pl/pik/restful/ScheduleController.java
+++ src/main/java/com/pl/pik/restful/ScheduleController.java
@@ -2,6 +2,7 @@ package com.pl.pik.restful;
 
 import com.pl.pik.model.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,7 +10,6 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/schedules")
-
 public class ScheduleController {
 
     @Autowired
@@ -21,12 +21,18 @@ public class ScheduleController {
     }
 
     @RequestMapping("/save")
-    public boolean scheduleSave(@RequestBody Schedule scheduleToSave){
-        if (isCollidindgWithSchedulesInDb(scheduleToSave)){
-            return false;
-        } else{
+    public ResponseEntity scheduleSave(@RequestBody Schedule scheduleToSave){
+        try {
+            checkCollisionsWithDbSchedules(scheduleToSave);
             scheduleRepository.save(scheduleToSave);
-            return true;
+            return ResponseEntity
+                    .status(HttpStatus.CREATED)
+                    .body(scheduleToSave.toString());
+        } catch(ScheduleOperationException e) {
+            e.printStackTrace();
+            return ResponseEntity
+                    .status(HttpStatus.FORBIDDEN)
+                    .body(e.getMessage());
         }
     }
 
@@ -46,20 +52,21 @@ public class ScheduleController {
 
     }
 
-    private boolean isCollidindgWithSchedulesInDb(Schedule scheduleToSave){
+    private void checkCollisionsWithDbSchedules(Schedule scheduleToSave) throws ScheduleOperationException {
         List<Schedule> busyDrivers= scheduleRepository.findByDriver(scheduleToSave.getDriver());
-        isScheduleDateColidingWithSchedules(scheduleToSave,busyDrivers);
+        if(isDateColiding(scheduleToSave,busyDrivers))
+            throw new ScheduleOperationException("Given driver is busy at the time");
 
         List<Schedule> busyCars = scheduleRepository.findByCar(scheduleToSave.getCar());
-        isScheduleDateColidingWithSchedules(scheduleToSave,busyCars);
+        if(isDateColiding(scheduleToSave,busyCars))
+            throw new ScheduleOperationException("Given car is busy at the time");
 
         List<Schedule> busyPassengers = scheduleRepository.findByPassenger(scheduleToSave.getPassenger());
-        isScheduleDateColidingWithSchedules(scheduleToSave,busyPassengers);
-
-        return false;
+        if(isDateColiding(scheduleToSave,busyPassengers))
+            throw new ScheduleOperationException("Given passenger is busy at the time");
     }
 
-    private boolean isScheduleDateColidingWithSchedules(Schedule scheduleToCompare, List<Schedule> schedules){
+    private boolean isDateColiding(Schedule scheduleToCompare, List<Schedule> schedules){
         for (Schedule schedule:schedules){
             if(! (schedule.getDateTo().before(scheduleToCompare.getDateFrom()) || schedule.getDateFrom().after(scheduleToCompare.getDateTo())))
                 return true;
---
+++ src/main/java/com/pl/pik/restful/ScheduleOperationException.java
@@ -0,0 +1,7 @@
+package com.pl.pik.restful;
+
+public class ScheduleOperationException extends Exception {
+    public ScheduleOperationException(String errorMessage) {
+        super(errorMessage);
+    }
+}
