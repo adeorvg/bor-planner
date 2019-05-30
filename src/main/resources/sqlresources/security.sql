169f154abee19150085bcd51843c4905cdadcf9c
Adrian Zach <adrien997@gmail.com>
2019-05-30 23:21:49 +0200

    added missing authorities values
    Change-Id: Ib90ee66ec685292834b4a28750d3eef1dba0c623

--- src/main/resources/sqlresources/security.sql
+++ src/main/resources/sqlresources/security.sql
@@ -4,4 +4,42 @@ create table authorities (
                            constraint fk_authorities_users foreign key(username) references users(username));
 
 insert into authorities(username,authority)
+values('astefaniak','ROLE_ADMIN');
+insert into authorities(username,authority)
+values('bkowalski','ROLE_ADMIN');
+insert into authorities(username,authority)
+values('bkowalewicz','ROLE_ADMIN');
+insert into authorities(username,authority)
+values('bwielski','ROLE_ADMIN');
+insert into authorities(username,authority)
 values('jnowak','ROLE_ADMIN');
+insert into authorities(username,authority)
+values('kadamczyk','ROLE_ADMIN');
+insert into authorities(username,authority)
+values('kwolny','ROLE_ADMIN');
+insert into authorities(username,authority)
+values('mklos','ROLE_ADMIN');
+insert into authorities(username,authority)
+values('mnietoperz','ROLE_ADMIN');
+insert into authorities(username,authority)
+values('mkowalski','ROLE_ADMIN');
+insert into authorities(username,authority)
+values('mwysoki','ROLE_ADMIN');
+insert into authorities(username,authority)
+values('mnowakowski','ROLE_ADMIN');
+insert into authorities(username,authority)
+values('mchlopski','ROLE_ADMIN');
+insert into authorities(username,authority)
+values('mwiatr','ROLE_ADMIN');
+insert into authorities(username,authority)
+values('pbosak','ROLE_ADMIN');
+insert into authorities(username,authority)
+values('sniski','ROLE_ADMIN');
+insert into authorities(username,authority)
+values('skozlowski','ROLE_ADMIN');
+insert into authorities(username,authority)
+values('sworkowski','ROLE_ADMIN');
+insert into authorities(username,authority)
+values('tniewiem','ROLE_ADMIN');
+insert into authorities(username,authority)
+values('wkrosno','ROLE_ADMIN');
