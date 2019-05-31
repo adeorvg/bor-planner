dad2c13634b3aa6581c55b92347eb6c7c222e1f8
kdeszcz <kdeszcz@bluesoft.net.pl>
2019-05-31 07:39:30 +0200

    SaveScheduleExceptionCause (front)
    Change-Id: Ib1df484d28931419a093f3601e62355ac4c5a6ef

---
+++ src/adminPanelGui/src/app/model/saveScheduleExceptionCause.ts
@@ -0,0 +1,5 @@
+export enum SaveScheduleExceptionCause {
+    BUSY_PASSENGER = "Pasażer ma już zaplanowany inny przejazd w tym czasie",
+    BUSY_DRIVER = "Kierowca będzie wykonywał inny przejazd w tym czasie",
+    BUSY_CAR = "Samochód w tym czasie jest zajety"
+}
